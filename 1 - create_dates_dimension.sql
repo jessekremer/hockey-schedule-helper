584529568bfa5dadb71e2bff574d7dc5c2a222c5
jessekremer <68323854+jessekremer@users.noreply.github.com>
2021-10-06 13:35:50 +0800

    Update 1 - create_dates_dimension.sql
    fixing date ranges

--- 1 - create_dates_dimension.sql
+++ 1 - create_dates_dimension.sql
@@ -15,16 +15,17 @@ END;
 
 call loop_date();
 
-update dates set matchup = 2 where day_date between str_to_date('19/10/2021','%d/%m/%Y') and str_to_date('25/10/2021','%d/%m/%Y');
-update dates set matchup = 3 where day_date between str_to_date('26/10/2021','%d/%m/%Y') and str_to_date('01/11/2021','%d/%m/%Y');
-update dates set matchup = 4 where day_date between str_to_date('02/11/2021','%d/%m/%Y') and str_to_date('08/11/2021','%d/%m/%Y');
-update dates set matchup = 5 where day_date between str_to_date('09/11/2021','%d/%m/%Y') and str_to_date('15/11/2021','%d/%m/%Y');
-update dates set matchup = 6 where day_date between str_to_date('16/11/2021','%d/%m/%Y') and str_to_date('22/11/2021','%d/%m/%Y');
-update dates set matchup = 7 where day_date between str_to_date('23/11/2021','%d/%m/%Y') and str_to_date('29/11/2021','%d/%m/%Y');
-update dates set matchup = 8 where day_date between str_to_date('30/11/2021','%d/%m/%Y') and str_to_date('06/12/2021','%d/%m/%Y');
-update dates set matchup = 9 where day_date between str_to_date('07/12/2021','%d/%m/%Y') and str_to_date('13/12/2021','%d/%m/%Y');
-update dates set matchup = 10 where day_date between str_to_date('14/12/2021','%d/%m/%Y') and str_to_date('20/12/2021','%d/%m/%Y');
-update dates set matchup = 11 where day_date between str_to_date('21/12/2021','%d/%m/%Y') and str_to_date('02/01/2022','%d/%m/%Y');
+update dates set matchup = 1 where day_date between str_to_date('12/10/2021','%d/%m/%Y') and str_to_date('17/10/2021','%d/%m/%Y');
+update dates set matchup = 2 where day_date between str_to_date('18/10/2021','%d/%m/%Y') and str_to_date('24/10/2021','%d/%m/%Y');
+update dates set matchup = 3 where day_date between str_to_date('25/10/2021','%d/%m/%Y') and str_to_date('31/10/2021','%d/%m/%Y');
+update dates set matchup = 4 where day_date between str_to_date('01/11/2021','%d/%m/%Y') and str_to_date('07/11/2021','%d/%m/%Y');
+update dates set matchup = 5 where day_date between str_to_date('08/11/2021','%d/%m/%Y') and str_to_date('14/11/2021','%d/%m/%Y');
+update dates set matchup = 6 where day_date between str_to_date('15/11/2021','%d/%m/%Y') and str_to_date('21/11/2021','%d/%m/%Y');
+update dates set matchup = 7 where day_date between str_to_date('22/11/2021','%d/%m/%Y') and str_to_date('28/11/2021','%d/%m/%Y');
+update dates set matchup = 8 where day_date between str_to_date('29/11/2021','%d/%m/%Y') and str_to_date('05/12/2021','%d/%m/%Y');
+update dates set matchup = 9 where day_date between str_to_date('06/12/2021','%d/%m/%Y') and str_to_date('12/12/2021','%d/%m/%Y');
+update dates set matchup = 10 where day_date between str_to_date('13/12/2021','%d/%m/%Y') and str_to_date('19/12/2021','%d/%m/%Y');
+update dates set matchup = 11 where day_date between str_to_date('20/12/2021','%d/%m/%Y') and str_to_date('02/01/2022','%d/%m/%Y');
 update dates set matchup = 12 where day_date between str_to_date('03/01/2022','%d/%m/%Y') and str_to_date('09/01/2022','%d/%m/%Y');
 update dates set matchup = 13 where day_date between str_to_date('10/01/2022','%d/%m/%Y') and str_to_date('16/01/2022','%d/%m/%Y');
 update dates set matchup = 14 where day_date between str_to_date('17/01/2022','%d/%m/%Y') and str_to_date('23/01/2022','%d/%m/%Y');
